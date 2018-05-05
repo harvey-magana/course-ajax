498d8b78e35ae2da35276824203099abaca40762
Harvey Magana <heyharv2012@gmail.com>
2018-05-05 15:10:20 -0700

    third commit, added the addImage function successful api call

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -3,8 +3,25 @@
     const searchField = document.querySelector('#search-keyword');
     let searchedForText = 'hippos';
     const unsplashRequest = new XMLHttpRequest();
-    
     const responseContainer = document.querySelector('#response-container');
+    unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+    unsplashRequest.onload = addImage;
+    unsplashRequest.setRequestHeader('Authorization', 'Client-ID 09e1b1366b815d72147f8c49359b63acd03a53e7013bef8f9515a255629d9010');
+    unsplashRequest.send();
+
+    function addImage() {
+    	let htmlContent = '';
+    	const data = JSON.parse(this.responseText);
+    	const firstImage = data.results[0];
+
+    	htmlContent = `<figure>
+    		<img src="${firstImage.urls.regular}" alt="${searchedForText}">
+    		<figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+    	</figure>`;
+
+    	responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+
+    }
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
