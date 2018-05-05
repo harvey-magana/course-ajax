f29b70d1042d5c42bf08a153c77e2da21fc94690
Harvey Magana <heyharv2012@gmail.com>
2018-05-05 14:40:27 -0700

    second commit, unspashRequest XHR object created

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -2,6 +2,7 @@
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
     let searchedForText = 'hippos';
+    const unsplashRequest = new XMLHttpRequest();
     
     const responseContainer = document.querySelector('#response-container');
 
